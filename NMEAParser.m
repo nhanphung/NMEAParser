f80a9eb8548b0dc145fb53a1da52383b5a101de1
Nhan Phung <nhanphung@HalaMadridBook.local>
2016-05-09 09:35:16 -0500

    updated saving output file

--- NMEAParser.m
+++ NMEAParser.m
@@ -23,21 +23,24 @@ T = cell(rows, 5);
 
 % parsing useful information
 for i = 1:rows
-    time = getTime(GPS.Var2(i));
-    date = getDate(GPS.Var10(i));
-    lat = getLat(GPS.Var4(i), GPS.Var5(i));
-    lon = getLon(GPS.Var6(i), GPS.Var7(i));
-    speed = getSpeed(GPS.Var8(i));
-    T{i,1} = time;
-    T{i,2} = date;
-    T{i,3} = round(lat*numOfDec)/numOfDec;
-    T{i,4} = round(lon*numOfDec)/numOfDec;
-    T{i,5} = speed;
+    check = char(GPS.Var1(i));
+    if (isempty(strfind(check, '$GPRMC')) == false)
+        time = getTime(GPS.Var2(i));
+        date = getDate(GPS.Var10(i));
+        lat = getLat(GPS.Var4(i), GPS.Var5(i));
+        lon = getLon(GPS.Var6(i), GPS.Var7(i));
+        speed = getSpeed(GPS.Var8(i));
+        T{i,1} = time;
+        T{i,2} = date;
+        T{i,3} = round(lat*numOfDec)/numOfDec;
+        T{i,4} = round(lon*numOfDec)/numOfDec;
+        T{i,5} = speed;
+    end
 end
 
 % convert cell array to a table with appropriate headers
 outputFile = array2table(T, 'VariableNames', {'time', 'date', 'latitude', 'longitude', 'speed'});
-outputFileName = ['PARSED_' fileName];
+[outputFileName, outputPath] =  uiputfile(['PARSED_' fileName]);
 % write table to another text file
-writetable(outputFile, outputFileName);
+writetable(outputFile, [outputPath outputFileName]);
 disp(['Successfully parsed GPS data. Output file is: ' outputFileName]);
